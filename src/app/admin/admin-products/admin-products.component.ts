acc137c2a6258cdf5d7c4cc3513e6d22090a2e5b
Sohel Rana <srana@leapinglogic.com>
2018-04-29 14:25:00 +0600

    data-table added

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../../product.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/take'
+import { Subscription } from 'rxjs/Subscription';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product[];
+  filteredProducts: any[];
+  subscription : Subscription;
+  product = {};
+  id;
+  constructor(private productService : ProductService,
+    private router : Router, 
+    private route : ActivatedRoute,
+    )  {
+    this.subscription = this.productService.getAll().subscribe(products => this.filteredProducts = this.products = products);
+    this.id = this.route.snapshot.paramMap.get('id');
+   }
 
-  constructor() { }
+   delete(){
+      if(confirm('Are you sure you want to delete this product?')) {
+        this.productService.delete(this.id);
+        this.router.navigate(['/admin/products']);
+      }else return;
+   
+  }
+
+  filter(query : string){
+   this.filteredProducts = (query) ?
+   this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+   this.products;
+
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 
   ngOnInit() {
   }
+
 
 }

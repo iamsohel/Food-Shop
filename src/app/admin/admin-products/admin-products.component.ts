8f21abbdcf1cbcc6b7de28f4e6d6b8219a0ee5be
Sohel Rana <srana@leapinglogic.com>
2018-04-29 16:45:06 +0600

    dt search

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -13,54 +13,43 @@ import { DataTableResource } from 'angular5-data-table';
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
-  filteredProducts: any[];
-  subscription : Subscription;
-  tableResource : DataTableResource<Product>;
-  items : Product[] = [];
-  itemCount : number;
-  product = {};
-  id;
-  constructor(private productService : ProductService,
-    private router : Router, 
-    private route : ActivatedRoute,
-    )  {
+  subscription: Subscription;
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number; 
+
+  constructor(private productService: ProductService) { 
     this.subscription = this.productService.getAll()
-    .subscribe(products =>{
-      this.filteredProducts = this.products = products;
-      this.initializeTable(products);
-    });
-    this.id = this.route.snapshot.paramMap.get('id');
-   }
+      .subscribe(products => {
+        this.products = products;
+        this.initializeTable(products);
+      });
+  }
 
-   private initializeTable(products : Product[]){
+  private initializeTable(products: Product[]) {
     this.tableResource = new DataTableResource(products);
-    this.tableResource.query({offset: 0})
-    .then(items => this.items =items);
-    this.tableResource.count().then(count =>this.itemCount = count);
-   }
+    this.tableResource.query({ offset: 0 })
+      .then(items => this.items = items);
+    this.tableResource.count()
+      .then(count => this.itemCount = count);
+  }
 
-   reloadItems(params){
-     if(!this.tableResource) return;
+  reloadItems(params) {
+    if (!this.tableResource) return;
+
     this.tableResource.query(params)
-    .then(items => this.items =items);
-   }
-
-   delete(){
-      if(confirm('Are you sure you want to delete this product?')) {
-        this.productService.delete(this.id);
-        this.router.navigate(['/admin/products']);
-      }else return;
-   
+      .then(items => this.items = items);    
   }
 
-  filter(query : string){
-   this.filteredProducts = (query) ?
-   this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
-   this.products;
+  filter(query: string) { 
+    let filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
 
+    this.initializeTable(filteredProducts);
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
